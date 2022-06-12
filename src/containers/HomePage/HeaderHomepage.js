d964e35b3555a4b0c9ec52012008e00756b8cc70
vinhphan812 <vinhphan812@gmail.com>
2022-06-12 21:07:01 +0700

    sync

--- src/containers/HomePage/HeaderHomepage.js
+++ src/containers/HomePage/HeaderHomepage.js
@@ -1,67 +1,82 @@
-import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './HeaderHomepage.scss';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "./HeaderHomepage.scss";
 class HeaderHomepage extends Component {
+	constructor(props) {
+		super(props);
+		console.log(this.props);
+	}
 
-    render() {     
-        return (
-         <>   
-            <div className='above'></div>                
-            <div className='home-header-container'>
-                <div className='home-header-content'>
-                        <div className='left-content'>
-                            <i className="fas fa-bars"></i>
-                            <div className='header-logo'>
-
-                            </div>
-                        </div>
-                        <div className='right-content'>
-                            <div className='child-content'>
-                                <p>Download App</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>Partnet with Us</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>Save</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>My Booking</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>EN</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>Pay</p>
-                            </div>
-                            <div className='child-content'>
-                                <p>Log In</p>
-                            </div>
-                            <div className='child-content'>
-                            <button type="button" className="btn btn-primary">Register</button>
-                            </div>
-                        </div>
-                </div>
-            </div>
-            <div className='home-header-banner'>
-            </div>   
-        </>
-            
-        );
-    }
-
+	render() {
+		return (
+			<>
+				<div className="above"></div>
+				<div className="home-header-container">
+					<div className="home-header-content">
+						<div className="left-content">
+							<i className="fas fa-bars"></i>
+							<div className="header-logo"></div>
+						</div>
+						<div className="right-content">
+							<div className="child-content">
+								<p>Download App</p>
+							</div>
+							<div className="child-content">
+								<p>Partnet with Us</p>
+							</div>
+							<div className="child-content">
+								<p>Save</p>
+							</div>
+							<div className="child-content">
+								<p>My Booking</p>
+							</div>
+							<div className="child-content">
+								<p>EN</p>
+							</div>
+							<div className="child-content">
+								<p>Pay</p>
+							</div>
+							{this.props.user ? (
+								<>
+									<a href="https://profile.vinhphancommunity.xyz/profile/view">
+										{this.props.user.name}
+									</a>
+									<button>Logout</button>
+								</>
+							) : (
+								<>
+									<div className="child-content">
+										<p>Log In</p>
+									</div>
+									<div className="child-content">
+										<button
+											type="button"
+											className="btn btn-primary"
+										>
+											Register
+										</button>
+									</div>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className="home-header-banner"></div>
+			</>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.admin.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+	return {
+		isLoggedIn: state.admin.isLoggedIn,
+		user: state.app.user,
+	};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+	return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderHomepage);
